2918148569b1e490b2ab252f5570ddcb684294b5
Pierre Zhou <sharkpi.zhou@gmail.com>
2025-03-18 16:14:43 +0100

    intiial commit

---
+++ TpDevOPS/src/test/java/com/example/TpDevOPS/TpDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TpDevOPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
